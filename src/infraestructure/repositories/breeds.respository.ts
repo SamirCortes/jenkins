54b9eb5c45e602b6605987a9e816b905605d6a51
SamirCortesSoho <edwin@soho.com.uy>
2025-04-24 17:52:46 -0500

    cambio

--- src/infraestructure/repositories/breeds.respository.ts
+++ src/infraestructure/repositories/breeds.respository.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common';
 import { BreedsOutputPort } from '@core/application/ports/output/breeds-output.port';
 import { BreedApi } from '../adapters/breeds/breed.api';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class BreedsRepository implements BreedsOutputPort {
-    constructor(private readonly _breedService: BreedApi) { }
+  constructor(private readonly _breedService: BreedApi) {}
 
-    async getAllBreeds(): Promise<any> {
-        return await this._breedService.getAllBreeds().then((data) => data)
-    }
+  async getAllBreeds(): Promise<any> {
+    return await this._breedService.getAllBreeds().then((data) => data);
+  }
 
-    async getBreedById(id: string): Promise<any> {
-        return await this._breedService.getBreedById(id).then((data) => data)
-    }
+  async getBreedById(id: string): Promise<any> {
+    return await this._breedService.getBreedById(id).then((data) => data);
+  }
 
-    async getBreedBySearch(search: string): Promise<any> {
-        return await this._breedService.getBreedBySearch(search).then((data) => data)
-    }
+  async getBreedBySearch(search: string): Promise<any> {
+    return await this._breedService
+      .getBreedBySearch(search)
+      .then((data) => data);
+  }
 
-    async getBreedByImages(image: string): Promise<any> {
-        return await this._breedService.getBreedByImages(image).then((data) => data)
-    }
-
+  async getBreedByImages(image: string): Promise<any> {
+    return await this._breedService
+      .getBreedByImages(image)
+      .then((data) => data);
+  }
 }
